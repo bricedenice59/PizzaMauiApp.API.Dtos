e3fb2ee4da4c1703b1c2de07ad8795e2b602a7b6
Brice Grenard <brice.grenard@gmail.com>
2024-01-28 12:56:18 +0100

    Add POCO related to authentication tokens.

---
+++ Dtos/TokenModelDto.cs
@@ -0,0 +1,7 @@
+namespace PizzaMauiApp.API.Dtos;
+
+public class TokenModelDto
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
--- Dtos/UserIdentityDto.cs
+++ Dtos/UserIdentityDto.cs
@@ -3,8 +3,8 @@ namespace PizzaMauiApp.API.Dtos;
 public class UserIdentityDto
 {
     public Guid Id { get; set; }
-    public string Token { get; set; }
     public string FirstName { get; set; }
     public string Email { get; set; }
-    
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
 }
