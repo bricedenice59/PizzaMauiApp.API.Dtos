a1377e5a6d47c61a71b1f372f52ffe8da4e33608
Brice Grenard <brice.grenard@gmail.com>
2024-01-30 18:31:31 +0100

    Update Dtos for cart/basket orders.

--- Dtos/CustomerBasketDto.cs
+++ Dtos/CustomerBasketDto.cs
@@ -2,6 +2,6 @@ namespace PizzaMauiApp.API.Dtos;
 
 public class CustomerBasketDto
 {
-    public required Guid Id { get; set; }
-    public List<BasketItemDto> Items { get; set; }
+    public required string CustomerId { get; set; }
+    public List<BasketItemDto> Items { get; set; } = new();
 }
